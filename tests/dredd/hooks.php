e2eff40e77fcd6c2524b758fb8b68d66deac34dc
Kasper Garnaes <kasper.garnaes@gmail.com>
2019-08-05 12:26:01 +0200

    Update hooks definition to satisfy PHPStan
    Make it PSR-4 compliant and remove unused use declaration.

--- tests/dredd/hooks.php
+++ tests/dredd/hooks.php
@@ -21,14 +21,13 @@ $client = new GuzzleHttp\Client([
 /**
  * Replace elements in requested path.
  */
-function pathReplace($transaction, $from, $to)
-{
+$pathReplace = function ($transaction, $from, $to) {
     $replacements = [$from => $to];
     $transaction->request->uri = strtr($transaction->request->uri, $replacements);
     $transaction->fullPath = strtr($transaction->fullPath, $replacements);
     // Also fix the ID so the user can see the change.
     $transaction->id = strtr($transaction->id, $replacements);
-}
+};
 
 /* @var \Laravel\Lumen\Application $app */
 $app = require __DIR__ . '/../../bootstrap/app.php';
@@ -50,19 +49,19 @@ Hooks::beforeEach(function ($transaction) {
 });
 
 // Change list id to trigger 404.
-Hooks::before('/list/{listId} > GET > 404', function (&$transaction) {
-    pathReplace($transaction, 'default', 'bad-value');
+Hooks::before('/list/{listId} > GET > 404', function (&$transaction) use ($pathReplace) {
+    $pathReplace($transaction, 'default', 'bad-value');
 });
 
 // Ensure material exists.
-Hooks::before('/list/{listId}/{materialId} > GET > 201', function (&$transaction) use ($client) {
+Hooks::before('/list/{listId}/{materialId} > GET > 201', function (&$transaction) use ($client, $pathReplace) {
     $client->put('/list/default/870970-basis%3A54871910-test-get', []);
 
     // Change the path to not clobber other tests.
-    pathReplace($transaction, '870970-basis%3A54871910', '870970-basis%3A54871910-test-get');
+    $pathReplace($transaction, '870970-basis%3A54871910', '870970-basis%3A54871910-test-get');
 });
 
 // Make sure list doesn't exist.
-Hooks::before('/list/{listId}/{materialId} > PUT > 404', function (&$transaction) use ($client) {
-    pathReplace($transaction, 'default', 'bad-value');
+Hooks::before('/list/{listId}/{materialId} > PUT > 404', function (&$transaction) use ($pathReplace) {
+    $pathReplace($transaction, 'default', 'bad-value');
 });
