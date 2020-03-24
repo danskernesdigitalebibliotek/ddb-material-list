1833b7b05ad5576dbea60c04652fe106219c087d
Kasper Garnaes <kasper.garnaes@gmail.com>
2020-03-25 00:02:30 +0100

    Update migration controller to use UPDATE IGNORE for migrations
    This way duplicate entries are ignored. In the current state they
    result in duplicate key errors.
    
    This query syntax. is not supporte by Illuminate/DB to we have to 
    rely on writing the query by hand. There is also a different syntax 
    for SQLite which we have to take into account for testing.

--- app/Http/Controllers/MigrateController.php
+++ app/Http/Controllers/MigrateController.php
@@ -12,9 +12,34 @@ class MigrateController extends Controller
     public function migrate(Request $request, string $openlistId)
     {
         // The "legacy-" prefix protects against high-jacking from another GUID.
-        $materials = DB::table('materials')
-            ->where(['guid' => 'legacy-' . $openlistId])
-            ->update(['guid' => $request->user()->getId()]);
+        // We have to use a raw query here as Illuminate/DB does not support
+        // UPDATE IGNORE. We need this to avoid duplicate entry errors if the
+        // user has added an item on a list manually which also exists in
+        // the materials to be migrated.
+        $query = "UPDATE IGNORE materials SET guid=:guid WHERE guid=:legacy";
+
+        // SQLite which we use for testing requires has a bit different syntax
+        // for updates which should ignore duplicate key conflicts.
+        $database = config('database.default');
+        if ($database) {
+            $driver = config("database.connections.${database}.driver");
+        } else {
+            // Config does not seem to work for Dredd tests. Fall back to
+            // environment variable checking.
+            $driver = env('DB_CONNECTION');
+        }
+
+        if ($driver == "sqlite") {
+            $query = "UPDATE OR IGNORE materials SET guid=:guid WHERE guid=:legacy";
+        }
+
+        DB::statement(
+            $query,
+            [
+                ':legacy' => 'legacy-' . $openlistId,
+                ':guid' => $request->user()->getId()
+            ]
+        );
 
         // Always return success.
         return new Response('', 204);
