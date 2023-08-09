352ca188a6bc6b76d2aa3473add56131b2fea9e2
Jesper Kristensen <cableman@linuxdev.dk>
2023-08-09 09:25:00 +0200

    ITK: Update run docker build script

--- infrastructure/docker/run.sh
+++ infrastructure/docker/run.sh
@@ -8,7 +8,9 @@ APP_VERSION=develop
 VERSION=latest
 
 docker build --pull --no-cache --build-arg APP_VERSION=${APP_VERSION} --tag=danskernesdigitalebibliotek/material-list:${VERSION} --file="material-list/Dockerfile" material-list
-docker build --pull --no-cache --build-arg VERSION=${VERSION} --tag=danskernesdigitalebibliotek/material-list-nginx:${VERSION} --file="nginx/Dockerfile" nginx
+
+docker pull nginxinc/nginx-unprivileged:alpine
+docker build --no-cache --build-arg VERSION=${VERSION} --tag=danskernesdigitalebibliotek/material-list-nginx:${VERSION} --file="nginx/Dockerfile" nginx
 
 docker push danskernesdigitalebibliotek/material-list:${VERSION}
 docker push danskernesdigitalebibliotek/material-list-nginx:${VERSION}
