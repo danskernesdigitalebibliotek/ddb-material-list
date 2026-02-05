7a0cfcbc251dda5180ff193607ad3ff8343a6743
Thomas Fini Hansen <xen@xen.dk>
2026-02-05 19:12:50 +0100

    Update to Lumen 11

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,9 +2,7 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-(new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(
-    dirname(__DIR__)
-))->bootstrap();
+
 
 /*
 |--------------------------------------------------------------------------
